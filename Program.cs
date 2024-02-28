d35ed2d7bd4f6f2cbd8cf026ead2a7343ee73c23
Jared Tims <jstims9109@gmail.com>
2024-02-27 20:50:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Console_Project_Dice_Rolls_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Chapter 10 Console Project Dice Rolls Array by Jared Tims\n");
+
+
+
+            WriteLine("\nPress any key to continue...");
+            ReadKey();
+        }
+    }
+}
